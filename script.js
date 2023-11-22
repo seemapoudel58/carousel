c43a2eb8d568d0fb00a75c63275817221cb54675
Seema <simapoudel6@gmail.com>
2023-11-22 15:46:32 +0545

    update

--- script.js
+++ script.js
@@ -1,52 +1,21 @@
-const sliderImages = document.querySelectorAll('.slide');
-const arrowLeft = document.querySelector('#arrow-left');
-const arrowRight = document.querySelector('#arrow-right');
-let current = 0;
+    const arrowLeft = document.getElementById('arrow-left');
+    const arrowRight = document.getElementById('arrow-right');
+    const slides = document.querySelectorAll('.slide');
 
-function reset() {
-  for (let i = 0; i < sliderImages.length; i++) {
-    sliderImages[i].style.display = 'none';
-  }
-}
+    let currentSlide = 0;
 
-function startSlide() {
-  reset();
-  sliderImages[current].style.display = 'block';
-}
+    arrowLeft.addEventListener('click', () => {
+        showSlide(currentSlide - 1);
+    });
 
-//show prev
-function slideLeft() {
-  if (current === 0) {
-    current = sliderImages.length - 1;
-  } else {
-    current--;
-  }
-  reset();
-  sliderImages[current].style.display = 'block';
-}
+    arrowRight.addEventListener('click', () => {
+        showSlide(currentSlide + 1);
+    });
 
-function slideRight() {
-  if (current === sliderImages.length - 1) {
-    current = 0;
-  } else {
-    current++;
-  }
-  reset();
-  sliderImages[current].style.display = 'block';
-}
+    function showSlide(index) {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (index + slides.length) % slides.length;
+        slides[currentSlide].classList.add('active');
+    }
 
-
-arrowLeft.addEventListener('click', slideLeft);
-
-arrowRight.addEventListener('click', slideRight);
-
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowLeft') {
-    slideLeft();
-  } else if (event.key === 'ArrowRight') {
-    slideRight();
-  }
-});
-
-startSlide();
-
+    showSlide(0);
